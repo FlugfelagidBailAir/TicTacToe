7d3f52984b50b910c0410f467ac8dbf2457c648f
Einar Arnason <einsiarna@gmail.com>
2017-10-26 21:24:54 +0000

    Connected web server to index page

--- src/main/java/com/ru/hugb/Main.java
+++ src/main/java/com/ru/hugb/Main.java
@@ -1,4 +1,18 @@
 package com.ru.hugb;
+
 import static spark.Spark.*;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 public class Main {
+    public static void main(String[] args) {
+
+    	TicTacToe game = new TicTacToe();
+
+    	staticFileLocation("/public");
+
+        get("/:pos", (req, res) -> game.setXboardPos(Integer.parseInt(":pos")));
+
+	}
 }
