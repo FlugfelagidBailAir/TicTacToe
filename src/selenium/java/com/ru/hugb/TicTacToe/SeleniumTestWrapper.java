22234af777126ffa5f7bbd4f165f5d8a7b8f3b46
godi15 <godi15@ru.is>
2017-10-27 21:26:46 +0000

    Come on travis, you can do it

--- src/selenium/java/com/ru/hugb/TicTacToe/SeleniumTestWrapper.java
+++ src/selenium/java/com/ru/hugb/TicTacToe/SeleniumTestWrapper.java
@@ -2,26 +2,39 @@ package com.ru.hugb.TicTacToe;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public abstract class SeleniumTestWrapper {
+  //static WebDriver driver;
   static ChromeDriver driver;
+
   static String baseUrl;
   static String port;
 
   @BeforeClass
   public static void openBrowser() {
+    //System.setProperty("webdriver.gecko.driver", "/Users/Godi/Downloads/geckodriver");
+    //driver = new FirefoxDriver();
+    //baseUrl = "https://frozen-springs-99760.herokuapp.com/";
     driver = new ChromeDriver();
+
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     port = System.getenv("PORT");
     if (port == null) {
         port = "4567";
     }
-    baseUrl = "http://localhost:" + port;
+    baseUrl = "http://localhost:4567";
+
+    driver.get(baseUrl);
   }
 
   @AfterClass
--- src/selenium/java/com/ru/hugb/TicTacToe/TestTTT.java
+++ src/selenium/java/com/ru/hugb/TicTacToe/TestTTT.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestTTT extends SeleniumTestWrapper {
   @Test
